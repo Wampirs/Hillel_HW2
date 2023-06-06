b8f949e23c2b4a9618741b533be47eee9b5d6173
Wampirs <79167501+Wampirs@users.noreply.github.com>
2023-06-06 07:56:10 +0300

    Fixes v2

--- HW3/Models/Deck.cs
+++ HW3/Models/Deck.cs
@@ -80,12 +80,14 @@
 
     internal class CardComparer : IComparer<Card>
     {
-        public int Compare(Card? x, Card? y)
+        public int Compare(Card x, Card y)
         {
             if(x.Suit < y.Suit) return -1;
             if (x.Suit > y.Suit) return 1;
-            return 0;
+            if(x.Value < y.Value) return -1;
+            return 1;
         }
+
     }
 
     internal enum DeckType
--- HW3/Models/II/Computer.cs
+++ HW3/Models/II/Computer.cs
@@ -6,18 +6,4 @@
         {
         }
     }
-
-    internal class BlackJackComputer : Computer
-    {
-        public bool WantToTakeCard()
-        {
-            int sum = Hand.Cards.Sum(card => card.GetCost());
-            if (Hand.Cards.All(card => card.Value == CardValue.Ace)
-                || sum >= 21) return false;
-
-            if (sum >= 17) return false;
-            return true;
-
-        }
-    }
 }
